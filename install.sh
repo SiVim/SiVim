8d06d6e7c20978daee3108a8a57546e59fe67502
Alden Garcia <aldenjg33@gmail.com>
2021-11-13 16:26:49 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ downloadrepo() {
 	echo "Cloning Repo..."
 	git clone https://github.com/SiVim/SiVim ~/.config/nvim
 
+	yay -S code-minimap
+
 	echo "Downloading Vim-Plug..."
 	curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
